77e49063c0e012ce36504371aaf0324c54070d90
marcus lagerstedt <marcus lagerstedt>
2016-01-11 15:48:43 +0100

    Fib

--- Lager.hs
+++ Lager.hs
@@ -1,5 +1,11 @@
 -- Sumsq n returns 1*1 + 2*2 + ... + n*n
 
 sumsq :: Int -> Int
-sumsq n | n == 1    = 1
-        | otherwise = sumsq (abs (n)-1) + n*n
+sumsq 0 = 0
+sumsq n = sumsq (abs (n)-1) + n*n
+
+-- fib n computes the nth Fibonacci number
+
+fib :: Int -> Int 
+fib n | n `elem` [0, 1] = 1
+      | otherwise       = fib (n-1) + fib (n-2)
