e8435bbbf14b152223d7a655ef0540d11435fa4a
Philip Ekman <ekman1991@gmail.com>
2016-01-11 15:37:24 +0100

    Try two

--- Ekman.hs
+++ Ekman.hs
@@ -3,4 +3,8 @@ sumsq counter = sumsq' counter 0
 
 sumsq' :: Integer -> Integer -> Integer
 sumsq' counter sums | counter == 0 = sums
-sumsq' counter sums | sumsq' (counter - 1) (sums + counter * counter)
+sumsq' counter sums = sumsq' (counter - 1) (sums + counter * counter)
+
+sumsq2 :: Int -> Int
+sumsq2 1 = 1
+sumsq2 val = val * val + sumsq2 (val - 1)
