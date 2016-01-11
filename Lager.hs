fd697835b9df2af51f9225b63cfd194ddc5b2e68
marcus lagerstedt <marcus lagerstedt>
2016-01-11 16:20:58 +0100

    New

--- Lager.hs
+++ Lager.hs
@@ -18,3 +18,11 @@ duplicates :: Eq a => [a] -> Bool
 duplicates []     = False
 duplicates (x:xs) | x `elem` xs = True
                   | otherwise   = duplicates xs
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates []     = []
+removeDuplicates xs     = help xs xs
+   where help []     news = news
+         help (x:xs) news | x `elem` xs = help xs (newArray x news)
+                          | otherwise   = help xs news
+         newArray x xs = delete x xs
