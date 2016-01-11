084638ed66b1c7050e34500b6f7bba397fa71fcf
Sebastian Bellevik <bellevik@Sebastians-MacBook-Pro.local>
2016-01-11 16:11:48 +0100

    removeDuplicates works

--- Sebbe.hs
+++ Sebbe.hs
@@ -10,3 +10,11 @@ fib n = fib (n-1) + fib (n-2)
 duplicates :: Eq a => [a] -> Bool
 duplicates []     = False
 duplicates (x:xs) = x `elem` xs || duplicates xs
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates xs = removeDuplicates' xs []
+
+removeDuplicates' :: Eq a => [a] -> [a] -> [a]
+removeDuplicates' []     ys = ys
+removeDuplicates' (x:xs) ys | not (x `elem` ys) = removeDuplicates' xs (ys ++ [x])
+                            | otherwise = removeDuplicates' xs ys
