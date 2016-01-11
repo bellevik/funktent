ce82e3810b56f13f70019c1e74d10e537d89d8aa
Sebastian Bellevik <bellevik@Sebastians-MacBook-Pro.local>
2016-01-11 15:58:14 +0100

    Think it funk

--- Sebbe.hs
+++ Sebbe.hs
@@ -3,6 +3,10 @@ sumsq 1 = 1
 sumsq n = n * n + sumsq (n-1)
 
 fib :: Int -> Int
-fib 0 = 0
+fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
+
+duplicates :: Eq a => [a] -> Bool
+duplicates []     = False
+duplicates (x:xs) = x `elem` xs || duplicates xs
