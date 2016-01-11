daf317596569e64bacb85892889d4fdcaf16af67
marcus lagerstedt <marcus lagerstedt>
2016-01-11 16:50:51 +0100

    NEws

--- Lager.hs
+++ Lager.hs
@@ -26,3 +26,16 @@ removeDuplicates xs     = help xs xs
          help (x:xs) news | x `elem` xs = help xs (newArray x news)
                           | otherwise   = help xs news
          newArray x xs = delete x xs
+
+-- Occurrences in Lists
+occursIn :: Eq a => a -> [a] -> Bool
+occursIn x xs = (or . map (\y -> y == x)) xs
+
+allOccursIn :: Eq a => [a] -> [a] -> Bool
+allOccursIn xs ys = (and . map (\a -> occursIn a ys)) xs 
+
+sameElements :: Eq a => [a] -> [a] -> Bool
+sameElements xs ys = allOccursIn xs ys && allOccursIn ys xs
+
+numOccurrences :: Eq a => a -> [a] -> Int
+numOccurrences x xs = (length . filter (==True) . map (\y -> y == x)) xs
