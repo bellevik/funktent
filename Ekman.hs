1a9fd37e4ef200325b1be1b943dea017d9e98f85
Philip Ekman <ekman1991@gmail.com>
2016-01-11 16:46:18 +0100

    Not done yet

--- Ekman.hs
+++ Ekman.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 sumsq :: Integer -> Integer
 sumsq counter = sumsq' counter 0
 
@@ -16,3 +18,13 @@ fib n = fib (n - 2) + fib (n - 1)
 duplicates :: [Integer] -> Bool
 duplicates [] = False
 duplicates (x:xs) = x `elem` xs || duplicates xs
+
+occursIn :: Eq a => a -> [a] -> Bool
+occursIn val list = val `elem` list
+
+allOccurIn :: Eq a => [a] -> [a] -> Bool
+allOccurIn [] _ = True
+allOccurIn (x:xs) list = occursIn x list && allOccurIn xs list
+
+sameElements :: Eq a => [a] -> [a] -> Bool
+sameElements list1 list2 | length list1 == length list2 = 
