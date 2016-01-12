d0fef63fc25ea1ecf7039cbac7fa20f8aa22e0b6
Philip Ekman <ekman1991@gmail.com>
2016-01-12 19:58:26 +0100

    Copy

--- Ekman.hs
+++ Ekman.hs
@@ -43,3 +43,29 @@ numOccurrences x xs = length (filter (\y -> y == x) xs)
 bag :: String -> [(Char, Int)]
 bag str = zip (nonDup str) (map (\x -> numOccurrences x str) (nonDup str))
             where nonDup string = removeDuplicates string
+
+
+data Expr = Lit Int
+          | Add Expr Expr
+          | Sub Expr Expr
+
+eval :: Expr -> Int
+eval (Lit n)     = n
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Sub e1 e2) = eval e1 - eval e2
+
+showExpr :: Expr -> String
+showExpr (Lit n)     = show n
+showExpr (Add e1 e2) = "(" ++ showExpr e1 ++ "+" ++ showExpr e2 ++")"
+showExpr (Sub e1 e2) = "(" ++ showExpr e1 ++ "-" ++ showExpr e2 ++")"
+
+
+size :: Expr -> Int
+
+
+
+
+
+
+
+
