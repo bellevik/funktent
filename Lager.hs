d560cdda2e236e7f8383d8f28952196f6bb7e3ac
lagerstedtmarcus <marlag@student.chalmers.se>
2016-01-12 20:02:07 +0100

    I fixeded it

--- Lager.hs
+++ Lager.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Control.Monad
+--import ExprVar
 
 -- Sumsq n returns 1*1 + 2*2 + ... + n*n
 
@@ -69,8 +70,30 @@ helpB xs = do
     helpB (x:xs)
 
 bubblesort :: [Int] -> [Int]
-bubblesort (x:xs) = help [] xs x
-  where help tested (x:xs) y = 
+bubblesort (x:xs) = helpbubble [] xs x
+  where helpbubble tested []     y = tested
+        helpbubble tested (x:xs) y | x > y     = helpbubble (tested ++ [y]) xs (x)
+                                   | otherwise = helpbubble (tested ++ [x]) xs (y)
 
 repeat :: IO Bool -> IO () -> IO ()
 repeat x y = undefined
+
+data Expr = Lit Int
+          | Add Expr Expr
+          | Sub Expr Expr
+
+eval :: Expr -> Int
+eval (Lit n)     = n
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Sub e1 e2) = eval e1 - eval e2
+
+showExpr :: Expr -> String
+showExpr (Lit n)     = show n
+showExpr (Add e1 e2) = "(" ++ showExpr e1 ++ "+" ++ showExpr e2 ++")"
+showExpr (Sub e1 e2) = "(" ++ showExpr e1 ++ "-" ++ showExpr e2 ++")"
+
+size :: Expr -> Int 
+size exp = helpsize exp 0
+  where helpsize (Lit a)     n = n + 1 
+        helpsize (Add e1 e2) n = helpsize e1 (n) + helpsize e2 (n) + 1
+        helpsize (Sub e1 e2) n = helpsize e1 (n) + helpsize e2 (n) + 1
