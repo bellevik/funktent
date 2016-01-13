39192a7669f40a09e25ea3c83315dbe2aebb84f0
lagerstedtmarcus <marlag@student.chalmers.se>
2016-01-13 10:19:33 +0100

    2013

--- Lager2013.hs
+++ Lager2013.hs
@@ -2,47 +2,93 @@ myName :: IO ()
 myName = do
 	putStrLn "Lager"
 
--- ##### Question 1 ##### --
--- a
+-- 1ai.*****************************************************************************
+maxRainfall :: Int -> Float
+maxRainfall n = foldr (\x y -> max y $ rainfall x) 0 [1..n]
+   where maxRain x y = max y (rainfall x)
 
 
--- b
+rainfall :: Int -> Float
+rainfall f = fromIntegral(f) * 2.14 
+
+-- 1aii.*****************************************
+maxRainfalla :: Int -> Float
+maxRainfalla n = helpMaxR n 0
+  where helpMaxR 0 maxV = maxV
+        helpMaxR n maxV | rainfall n > maxV = helpMaxR (n-1) (rainfall n)
+                        | otherwise         = helpMaxR (n-1) maxV       
 
 
--- ##### Question 2 ##### --
--- a
+-- 1b *******************************************
+maxWeeks :: Int -> [Int]
+maxWeeks n = weeks n [] (maxRainfall n)
+  where weeks 0 xs a = xs
+        weeks n xs a | rainfall n == a = weeks (n-1) (n:xs) a
+                     | otherwise       = weeks (n-1) xs     a
 
 
--- b
+-- 2a ******************************************************************************
+--  fa :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+--  fa x y z = x z && y z
 
+-- fb :: Num a => a -> Maybe a
+-- fb m = Just (m+1)
 
--- c
+-- fc :: (Num a, Ord b) => [b] -> a
+-- fc (x:y:xs) | x > y = 999
+-- fc _                = 0
 
+-- fd :: Eq a => [[a]] -> [b] -> Bool
+-- fd (x:xs) (y:ys) = x == ys
+-- fd []     ys     = ys == []
 
--- ##### Question 3 ##### --
--- a
+-- fe :: Monad a => m a -> [a] -> m [a]
+-- fe x y = do
+--           z <- x
+--           return $ z:y
 
+-- 2b *******************************************
+-- ff x y = do
+--          c <- readFile x
+--          d <- readFile c
+--          putStr (c ++ "\n" ++ d)
 
--- b
+-- ff x y = readFile x >>= \c -> readFile c >>= \d-> putStr ( c ++ "\n" ++ d)
 
+-- 2c *******************************************
+--fg a tb tc td
+--   = case lookup a tb of
+--          Nothing -> Nothing
+--          Just b -> case lookup b tc of
+--                         Nothing -> Nothing
+--                         Just c -> case lookup c td of
+--                                        Nothing -> Nothing
+--                                        Just d -> Just [b,c,d]
+test2c :: a -> Maybe a
+test2c a = do
+   x <- Just a
+   Just x
 
--- c
+--fg' a tb tc td = do
+--      a <- lookup a tb
+--      b <- lookup a tc
+--      c <- lookup a td
+--      Just [a,b,c]
 
+-- 3 *******************************************************************************
 
--- d
+data Bag a = EmptyBag | Node a Int (Bag a) (Bag a)
 
+emptyBag :: Bag a
+emptyBag = EmptyBag
 
--- e
+-- 3a********************************************
+-- När man vill göma implementationen av datatypen ( det gör att man inte exporterar konstruktorn)
 
+-- 3b********************************************
 
--- ##### Question 4 ##### --
--- a
-
-
--- b
-
-
--- c
-
-
--- d
+bcount :: Ord a => a -> Bag a -> Int
+bcount a EmptyBag         = 0
+bcount a (Node x y x1 x2) |  x == a = y
+                          |  x > a  = bcount x1
+                          |  x < a  = bcount x2
